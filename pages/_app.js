3033446a21b0eba5c65bb20f9a1127f3834e7a1c
cr7cr8 <cr7cr8@126.com>
2022-04-21 17:06:12 +0800

    a

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import React, { useContext } from "react"
+import { context, ContextProvider } from "../ContextProvider"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ContextProvider>
+      <h2>aaa</h2>
+      <Component {...pageProps} />
+    </ContextProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/about.js
@@ -0,0 +1,25 @@
+
+
+import React, { useContext } from "react"
+import { Context, ContextProvider } from "../ContextProvider"
+
+import { Button } from "@mui/material"
+import Link from "next/link"
+
+
+export default function About() {
+
+
+    const { count, setCount } = useContext(Context)
+
+    return (
+
+        <>
+            <Link href="/" ><h1>go to Home {count}</h1></Link>
+            <Button variant="contained" onClick={function () {
+                setCount(pre => pre + 1)
+            }}>add</Button>
+        </>
+
+    )
+}
